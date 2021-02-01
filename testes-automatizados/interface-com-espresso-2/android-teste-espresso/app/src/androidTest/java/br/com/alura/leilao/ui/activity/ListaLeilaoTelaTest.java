a5c2839761d88d786ace3e87a5e2a4e4797996a8
Jucemar Vieira Junior <jucemarvlg@gmail.com>
2021-02-01 20:53:22 -0300

    Conclusão teste de interface com espresso

---
+++ testes-automatizados/interface-com-espresso-2/android-teste-espresso/app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
@@ -0,0 +1,92 @@
+package br.com.alura.leilao.ui.activity;
+
+import android.content.Intent;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import br.com.alura.leilao.BaseTesteIntegracao;
+import br.com.alura.leilao.R;
+import br.com.alura.leilao.api.retrofit.client.TesteWebClient;
+import br.com.alura.leilao.model.Leilao;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.*;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static br.com.alura.leilao.matchers.ViewMatcher.apareceLeilaoNaPosicao;
+
+public class ListaLeilaoTelaTest extends BaseTesteIntegracao {
+
+
+    @Rule
+    public ActivityTestRule<ListaLeilaoActivity> activity =
+            new ActivityTestRule<>(ListaLeilaoActivity.class, true, false);
+
+    @Before
+    public void setup() throws IOException {
+        limpaBancoDeDadosDaApi();
+    }
+
+    @Test
+    public void deve_AparecerUmLeilao_QuandoCarregarUmLeilaoNaApi() throws IOException {
+        tentaSalvarLeilaoNaApi(new Leilao("Carro"));
+
+        activity.launchActivity(new Intent());
+
+        onView(withId(R.id.lista_leilao_recyclerview))
+                .check(matches(apareceLeilaoNaPosicao(0, "Carro", 0.00)));
+    }
+
+    @Test
+    public void deve_AparecerDoisLeiloes_QuandoCarregarDoisLeiloesDaApi() throws IOException {
+        tentaSalvarLeilaoNaApi(
+                new Leilao("Carro"),
+                new Leilao("Computador"));
+
+        activity.launchActivity(new Intent());
+
+        onView(withId(R.id.lista_leilao_recyclerview))
+                .check(matches(apareceLeilaoNaPosicao(0, "Carro", 0.00)));
+
+        onView(withId(R.id.lista_leilao_recyclerview))
+                .check(matches(apareceLeilaoNaPosicao(1, "Computador", 0.00)));
+    }
+
+    @Test
+    public void deve_AparecerUltimoLeilao_QuandoCarregarDezLeiloesDaApi() throws IOException {
+        tentaSalvarLeilaoNaApi(
+                new Leilao("Carro"),
+                new Leilao("Computador"),
+                new Leilao("TV"),
+                new Leilao("Console"),
+                new Leilao("Notebook"),
+                new Leilao("Jogo"),
+                new Leilao("Estante"),
+                new Leilao("Quadro"),
+                new Leilao("Smartphone"),
+                new Leilao("Casa"));
+
+        activity.launchActivity(new Intent());
+        /*
+        * O método ViewActions.scrollTo() depende que a view passada para o onView, seja filha de uma
+        * scroll view. Para recycler view, por exemplo, não funcionaria.
+        * */
+        onView(withId(R.id.lista_leilao_recyclerview))
+                .perform(scrollToPosition(9))
+                .check(matches(apareceLeilaoNaPosicao(9, "Casa", 0.00)));
+    }
+
+    @After
+    public void teardown() throws IOException {
+        limpaBancoDeDadosDaApi();
+    }
+}
